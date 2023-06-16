24f3ed237182fc6d652156842d7b83fa9c97e488
j-miskill <jcmiskill@gmail.com>
2023-06-16 09:00:57 -0400

    added gui (tkinter) and started thinking about the next steps. I think I need to write down how I want to architecturre this shiz

--- logic/request_handler.py
+++ logic/request_handler.py
@@ -1,4 +1,7 @@
-# Handle requests to the API server for data.
+"""
+The reason this is OK is because I don't want to store all of the data on my disk. I want to write 
+a program that accesses that data and then changes/uses it.
+"""
 
 from os import link
 import pandas as pd
@@ -6,22 +9,17 @@ from sodapy import Socrata
 import requests
 import json
 
-
-# These are ways I have discovered to access the data better
-# value = requests.get("https://data.norfolk.gov/resource/jz6u-9g3c.json")
-# print(value.content)
-
-# client = Socrata("data.norfolk.gov", None)
-# results = client.get("jz6u-9g3c", limit=2000)
-
-
 class request_handler:
 
-    LINK_TO_API = "https://data.norfolk.gov/resource/jz6u-9g3c.json"
 
-    def __init__(self):
-        print("Initialized")
 
-    def get_data(self):
-        data = requests.post(self.LINK_TO_API)
-        return data.text
+    def __init__(self, user):
+        self.link = "https://data.norfolk.gov/resource/jz6u-9g3c.json"
+        self.user = user
+
+    def request_all_data(self):
+        data = requests.get(self.link)
+        return data
+
+
+
