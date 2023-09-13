599b4e44c040d811b7795f007e306cc130f01f3b
j-miskill <jcmiskill@gmail.com>
2023-09-12 21:10:09 -0400

    Adding in changes from video 8

--- sig_tree/sig_tree/settings.py
+++ sig_tree/sig_tree/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "trees-home"
 LOGIN_URL = "login"
+
+MEDIA_ROUTE = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
