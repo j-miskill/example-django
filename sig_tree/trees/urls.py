1393fc62f549c2846211345dc6e7f04819a75e08
j-miskill <jcmiskill@gmail.com>
2023-09-21 11:18:16 -0400

    Adding in the class views from the django tutorials

--- sig_tree/trees/urls.py
+++ sig_tree/trees/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("", views.home, name  = "trees-home"),
+    path("", PostListView.as_view(), name  = "trees-home"),
+    path("trees/<int:pk>/", PostDetailView.as_view(), name  = "post_detail"),
     path("about/", views.about, name = "trees-about")
 ]
--- sig_tree/trees/views.py
+++ sig_tree/trees/views.py
@@ -1,5 +1,6 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 # Create your views here.
 
@@ -18,6 +19,19 @@ posts = [
     }
 
 ]
+
+class PostListView(ListView):
+    model = Post
+    template_name = "trees/home.html"
+    context_object_name = 'posts'
+    ordering = ["-date_posted"]
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+
 
 def home(request):
     context =  {
