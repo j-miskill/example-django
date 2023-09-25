544ccc30d37c292d35611305512066f351f56db3
j-miskill <jcmiskill@gmail.com>
2023-09-25 14:35:12 -0400

    Video 9,10

--- sig_tree/trees/models.py
+++ sig_tree/trees/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
 
@@ -10,7 +11,10 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title          
+        return self.title     
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs = {"pk": self.pk})
 
 
 
--- sig_tree/trees/urls.py
+++ sig_tree/trees/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from . import views
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path("", PostListView.as_view(), name  = "trees-home"),
     path("trees/<int:pk>/", PostDetailView.as_view(), name  = "post_detail"),
+    path("trees/new/", PostCreateView.as_view(), name  = "post_create"),
+    path("trees/<int:pk>/update/", PostUpdateView.as_view(), name  = "post_update"),
+    path("trees/<int:pk>/delete/", PostDeleteView.as_view(), name  = "post_delete"),
     path("about/", views.about, name = "trees-about")
 ]
